5fee2018d7f1c4ceb7aae9e98b1a3d5499dc6544
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com>
2024-11-07 13:50:30 -0500

    Solution exercise five

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-#Ejercicio 4: Determinación del tipo de triángulo
-#Escribe un programa que determine el tipo de triángulo en función de sus lados usando if .
+#Ejercicio 5: Días de la semana
+#Escribe un programa que, dado un número del 1 al 7, imprima el día correspondiente de la
+#semana usando match .
 #Enunciado:
-#Solicita al usuario que ingrese las longitudes de los tres lados de un triángulo. Determina si el
-#triángulo es equilátero, isósceles o escaleno.
+#Solicita al usuario un número del 1 al 7 y muestra el día de la semana correspondiente (1 = Lunes,
+#7 = Domingo).
 
-a = int (input("Enter the first side: "))
-b = int(input("Enter the second side: "))
-c = int(input("Enter the third side: "))
-
-if a == b == c:
-    print("The triangle is equilateral") 
-if (a == b and b != c) or (a == c and b != c) or (c == b and a != c):
-    print("The triangle is isosceles")
-if a != b and b != c and c != a: 
-    print("The triangle is scalene")
+while True:
+    number = int(input("Enter a number to tell you what day is: "))
+    match number: 
+        case 1:
+            print("Sunday")
+        case 2:
+            print("Monday")
+        case 3:
+            print("Tuesday")
+        case 4:
+            print("Wednesday")
+        case 5:
+            print("Thursday")
+        case 6:
+            print("Friday")
+        case 7:
+            print("Saturday")
+        case _:
+            print("Thats not a day in the week")
