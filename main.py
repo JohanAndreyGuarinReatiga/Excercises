12413f2934d73f4897cc59e913bcc41fc1c979ed
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com>
2024-11-07 13:18:10 -0500

    Solution exercise four

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-#Ejercicio 2: Calificación de una nota
-#Escribe un programa que determine si una nota numérica es "Aprobado" o "Reprobado" usando
-#if .
+#Ejercicio 4: Determinación del tipo de triángulo
+#Escribe un programa que determine el tipo de triángulo en función de sus lados usando if .
 #Enunciado:
-#Solicita al usuario una calificación y determina si la nota es aprobatoria (>= 60) o reprobatoria (<
-#60).
+#Solicita al usuario que ingrese las longitudes de los tres lados de un triángulo. Determina si el
+#triángulo es equilátero, isósceles o escaleno.
 
-grade = int(input("Type your grade to define if youre approved or not: "))
+a = int (input("Enter the first side: "))
+b = int(input("Enter the second side: "))
+c = int(input("Enter the third side: "))
 
-if grade >= 60:
-    print("Your grade is passing")
-if grade: 
-    print("Your grade is failing")
+if a == b == c:
+    print("The triangle is equilateral") 
+if (a == b and b != c) or (a == c and b != c) or (c == b and a != c):
+    print("The triangle is isosceles")
+if a != b and b != c and c != a: 
+    print("The triangle is scalene")
