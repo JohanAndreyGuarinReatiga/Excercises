46a6987c854909b231d87b9355f470afd0a706b8
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-07 20:49:21 -0500

    Solution exercise six

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-#Ejercicio 3: Calculadora básica
-#Utiliza match para implementar una calculadora simple.
+#Ejercicio 6: Juego de adivinanza de números
+#Escribe un programa que implemente un juego de adivinanza de números.
 #Enunciado:
-#Crea una calculadora que solicite dos números y una operación matemática (+, -, *, /). Usa match
-#para realizar la operación correspondiente.
+#El programa genera un número aleatorio entre 1 y 10. El usuario debe adivinar el número, y el
+#programa indica si el número ingresado es mayor, menor o igual al número generado.
+import random
 
+number = random.randint(1,10)
 while True:
-    firstNumber = int(input("Enter the first number: "))
-    secondNumber = int(input("Enter the second number: "))
-    operator = input("Enter the operator (+, -, *, /):")
-    match operator:
-        case "+":
-            print(firstNumber + secondNumber)
-        case "-":
-            print(firstNumber - secondNumber)
-        case "*":
-            print(firstNumber * secondNumber)
-        case "/":
-            print(firstNumber / secondNumber)
-        case _:
-            print("Thats not an operator")
-    quest = input("do you want to continue? type yes or no: ")
-    if quest == "yes":
-        continue
-    else: 
+    guessing = int(input("try to guess the number: "))
+    if guessing > number:
+        print("your guessing is greater than the number")
+    if guessing < number:
+        print("your guessing is less than the number")
+    elif guessing == number:
+        print("your guessing is equal to the number, YOU WON!!!")
         break
+
