14a9148b29926c4b16f1a53b92e48a6d595235df
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-09 17:45:12 -0500

    Solution exercise thirteen

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-#Ejercicio 12: Calculadora de IMC (Índice de Masa Corporal)  BODY MASS INDEX (BMI)
-#Escribe un programa que calcule el IMC y determine el estado de peso.
+#Ejercicio 13: Comparación de tres números
+#Escribe un programa que determine el mayor de tres números usando if .
 #Enunciado:
-#Solicita al usuario su peso (en kg) y su altura (en metros). Calcula el IMC y clasifícalo en bajo peso
-#(<18.5), peso normal (18.5-24.9), sobrepeso (25-29.9), o obesidad (>=30).
-import math
-height = float(input("Enter you height in meters: "))
-weight = float(input("Enter you weight in kilos: "))
+#Solicita al usuario que ingrese tres números y determina cuál es el mayor.
 
-imc = weight / (math.pow(height,2))  
+firstNumber = int(input("Enter a number: "))
+secondNumber = int(input("Enter a number: "))
+thirdNumber = int(input("Enter a number: "))
 
-if imc <= 18.5:
-    print("Underweight")
-elif 18.5 <= imc <= 24.9:
-    print("Normal weight")
-elif 25 <= imc <= 29.9:
-    print("Overweight")
-elif imc >= 30: 
-    print("YOURE OBESE")
+if firstNumber >= secondNumber and firstNumber >= thirdNumber:
+    print(f"The largest number is: {firstNumber}")
+elif secondNumber >= firstNumber and secondNumber >= thirdNumber:
+    print(f"The largest number is: {secondNumber}")
+else:
+    print(f"The largest number is: {thirdNumber}")
