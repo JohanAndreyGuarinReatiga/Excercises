15b8b677223048816ae7064e8066524a59332077
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-11 09:49:09 -0500

    Solution exercise sixteen

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-#Ejercicio 15: Cálculo del salario neto
-#Escribe un programa que calcule el salario neto de un empleado después de aplicar impuestos.
+#Ejercicio 16: Cálculo del tiempo de viaje
+#Escribe un programa que calcule el tiempo que tarda en llegar un automóvil a su destino.
 #Enunciado:
-#Solicita al usuario su salario bruto y su país de residencia. Calcula el salario neto aplicando
-#impuestos: el 20% para residentes de "País A", el 15% para "País B" y el 10% para "País C". Si el
-#país no está en la lista, aplica un 25% de impuestos.
+#Solicita al usuario la distancia a recorrer (en km) y la velocidad promedio del automóvil (en km/h).
+#Calcula el tiempo de viaje en horas y minutos. Si la velocidad es mayor a 120 km/h, muestra un
+#mensaje de advertencia.
 
-grossSalary = int(input("Type your gross salary: "))
-country = input("Type the country were you live (A, B, C): ")
+distance = int(input("Enter the distance in kilometers: "))
+avSpeed = int(input("Enter the average speed in kilometers per hour: "))
 
-if country == "A":
-    taxRate = 0.20
-elif country == "B":
-    taxRate = 0.15
-elif country == "C":
-    taxRate = 0.10
-else:
-    taxRate = 0.25
-    
-netSalary = (grossSalary * (1 - taxRate))
-print(f"your net salary after taxaes is: ${netSalary}")
+time = distance / avSpeed
+
+hours = int(time) 
+minutes = int((time - hours) * 60) 
+if avSpeed > 120:
+    print("""
+                *********************************
+                            WARNING!!!
+                    YOURE EXCEEDING 120 km/h 
+                *********************************
+""")
+
+print(f"The travel time is {hours} hours and {minutes} minutes.")
