6ac8724921e54498544b0c9c2ffb26cf31d1227c
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-11 15:38:23 -0500

    Iterative Structures Exercise 1

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-#Ejercicio 22: Clasificación de triángulos por sus ángulos
-#Escribe un programa que clasifique un triángulo en agudo, obtuso o rectángulo según sus ángulos
-#internos usando if .
+#Ejercicio 1: Suma de los primeros N números enteros
 #Enunciado:
-#Solicita al usuario los tres ángulos de un triángulo y clasifícalo en:
-#Agudo: Todos los ángulos son menores a 90°.
-#Rectángulo: Un ángulo es exactamente 90°.
-#Obtuso: Un ángulo es mayor a 90°.
+#Escribe un programa que solicite al usuario un número entero positivo n y calcule la suma de los
+#primeros n números enteros. Utiliza un ciclo for para realizar la suma.
 
+sum = 0
 
-while True:
-    firstAngle = float(input("Enter the first angle: "))
-    secondAngle = float(input("Enter the second angle: "))
-    thirdAngle = float(input("Enter the third angle: "))
-    
-    if (firstAngle + secondAngle + thirdAngle) != 180:
-        print("Thats not a triangle, try again")
-    elif firstAngle < 90 and secondAngle < 90 and thirdAngle < 90:
-        print("The triangle is Acute")
-        break
-    elif firstAngle == 90 or secondAngle == 90 or thirdAngle == 90:
-        print("The triangle is Right")
-        break
-    else:
-        print("The triangle is Obtuse")
-        break
+n = int(input("Enter an integer positive number: "))
+for i in range(1, n+1):
+    sum += i
+
+print(sum)
