1945bd6f9907ddaced2cf459d6c965f0df3e6975
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-11 08:55:42 -0500

    Solution exercise fifteen

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-#Ejercicio 14: Adivinanza de letras
-#Escribe un programa que permita al usuario adivinar una letra secreta usando match .
+#Ejercicio 15: Cálculo del salario neto
+#Escribe un programa que calcule el salario neto de un empleado después de aplicar impuestos.
 #Enunciado:
-#El programa contiene una letra secreta (por ejemplo, "A"). El usuario debe adivinar la letra, y el
-#programa le indicará si acertó o no.
+#Solicita al usuario su salario bruto y su país de residencia. Calcula el salario neto aplicando
+#impuestos: el 20% para residentes de "País A", el 15% para "País B" y el 10% para "País C". Si el
+#país no está en la lista, aplica un 25% de impuestos.
 
-guessLetter = input("Guess the letter: ")
+grossSalary = int(input("Type your gross salary: "))
+country = input("Type the country were you live (A, B, C): ")
 
-match guessLetter:
-    case "A":
-        print("You guessed the secret letter")
-    case _:
-        print("Thats not the correct letter")
+if country == "A":
+    taxRate = 0.20
+elif country == "B":
+    taxRate = 0.15
+elif country == "C":
+    taxRate = 0.10
+else:
+    taxRate = 0.25
+    
+netSalary = (grossSalary * (1 - taxRate))
+print(f"your net salary after taxaes is: ${netSalary}")
