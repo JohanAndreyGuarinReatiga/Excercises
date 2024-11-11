b2b0dfdcfd88fe52fbe89480be21698efae598d7
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-11 11:18:32 -0500

    Solution exercise eighteen

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-#Ejercicio 17: Sistema de calificaciones con bonificaciones
-#Escribe un programa que calcule la calificación final de un estudiante basándose en su calificación
-#y si ha hecho tareas adicionales. Las tareas adicionales pueden darle un extra de puntos, pero el
-#máximo de puntos no puede exceder 100.
+#Ejercicio 18: Sistema de evaluación de créditos universitarios
+#Escribe un programa que calcule el número de créditos totales de un estudiante en base a las
+#materias cursadas y el puntaje obtenido en cada una. El puntaje debe ser evaluado como
+#aprobado o no aprobado.
 #Enunciado:
-#Solicita la calificación del estudiante y pregunta si hizo tareas adicionales. Si la respuesta es "sí",
-#añade un 5% extra a la calificación, pero si la calificación supera 100, ajústala a 100. Si la respuesta
-#es "no", simplemente muestra la calificación original
+#Solicita al usuario ingresar el número de materias que ha cursado. Para cada materia, solicita el
+#puntaje y determina si ha aprobado o no (>= 60). Luego, calcula el número total de créditos del
+#estudiante (cada materia aprobada otorga 3 créditos)
 
-grade = int(input("Write your grade: "))
-extraAssignments = input("have you done extra assignments?(yes or no): ")
-extraScore = grade + (grade * 0.05)
+numSubjects = int(input("How many subjects have you completed? write it here: "))
+credits = 0
 
-match extraAssignments: 
-    case "yes":
-        if extraScore < 100:
-            print(f"Your final score is {extraScore}")
-        elif extraScore > 100:    
-            print("Your final score is 100")
-    case _:
-        print(f"Your grade is {grade}")
+for i in range(numSubjects):
+    grade = float(input(f"what have you scored in the {i+1} subject?: "))
+    if grade >= 60:
+        credits += 3
+    
+print(f"Your total on credits is {credits}")
