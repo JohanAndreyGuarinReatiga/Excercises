dfa17248f69886050ee322d263b787ca20b329f5
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-12 06:01:11 -0500

    Iterative Structures Exercise 4

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-#Ejercicio 3: Factorial de un número
+#Ejercicio 4: Números pares en un rango
 #Enunciado:
-#Escribe un programa que solicite al usuario un número entero positivo n y calcule el factorial de
-#dicho número ( n! = 1 * 2 * 3 * ... * n ). Usa un ciclo for para realizar el cálculo.
+#Escribe un programa que solicite al usuario dos números enteros, un valor de inicio y un valor de
+#fin. El programa debe imprimir todos los números pares en ese rango, incluyendo los límites. Usa
+#un ciclo for para recorrer el rango
 
-n = int(input("Enter a number: "))
+numOne = int(input("Enter a number: "))
+numTwo = int(input("Enter a number: "))
 
-factor = 1
-for i in range(1, n + 1):
-    factor *= i
+if numOne % 2 != 0:
+    numOne += 1  
 
-print(f"The factorial of {n} is: {factor}")
+for i in range(numOne, numTwo + 1, 2):
+    print(i)
