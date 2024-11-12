3ce2f3138fb6f4f8fde67aac93f246092cd91b9c
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-12 06:15:34 -0500

    Iterative Structures Exercise 5

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-#Ejercicio 4: Números pares en un rango
+#Ejercicio 5: Días de la semana
+#Escribe un programa que, dado un número del 1 al 7, imprima el día correspondiente de la
+#semana usando match .
 #Enunciado:
-#Escribe un programa que solicite al usuario dos números enteros, un valor de inicio y un valor de
-#fin. El programa debe imprimir todos los números pares en ese rango, incluyendo los límites. Usa
-#un ciclo for para recorrer el rango
+#Solicita al usuario un número del 1 al 7 y muestra el día de la semana correspondiente (1 = Lunes,
+#7 = Domingo).
 
-numOne = int(input("Enter a number: "))
-numTwo = int(input("Enter a number: "))
-
-if numOne % 2 != 0:
-    numOne += 1  
-
-for i in range(numOne, numTwo + 1, 2):
-    print(i)
+while True:
+    number = int(input("Enter a number to tell you what day is: "))
+    match number: 
+        case 1:
+            print("Sunday")
+        case 2:
+            print("Monday")
+        case 3:
+            print("Tuesday")
+        case 4:
+            print("Wednesday")
+        case 5:
+            print("Thursday")
+        case 6:
+            print("Friday")
+        case 7:
+            print("Saturday")
+        case _:
+            print("Thats not a day in the week")
