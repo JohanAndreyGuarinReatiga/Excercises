f1b7a366d9129ca3ae849879bb867b703788535d
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-12 06:38:45 -0500

    Iterative Structures Exercise 7

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-#Ejercicio 6: Adivina el número (con while )
-#Enunciado: (random.randint(1, 100))
-#Escribe un programa que genere un número aleatorio entre 1 y 100 y permita al usuario
-#adivinarlo. El programa debe dar pistas si el número ingresado es mayor o menor que el número
-#secreto. Usa un ciclo while para permitir al usuario seguir intentando hasta que adivine el
-#número
+#Ejercicio 7: Suma de números pares hasta que se introduce un impar
+#Enunciado:
+#Escribe un programa que solicite al usuario ingresar números enteros indefinidamente. El
+#programa debe sumar los números siempre que sean pares, pero debe detenerse si el usuario
+#ingresa un número impar. Usa un ciclo while para lograr esto.
 
-import random
-
-number = random.randint(1,100)
+sum = 0
 while True:
-    guessing = int(input("try to guess the number: "))
-    if guessing > number:
-        print("your guessing is greater than the number")
-    if guessing < number:
-        print("your guessing is less than the number")
-    elif guessing == number:
-        print("\nYOU'VE GUESSED THE NUMBER!!!\n")
+    n = int(input("Enter a number: "))
+    if n %2 != 0:
         break
+    else:
+        sum += n
+        print(sum)
+        pass
